1c8d5a49a996f032e37f0f2ab75c4cd0da6d402d
amartyanambiar <amartyanambiar@gmail.com>
2023-03-23 14:53:00 +0530

    update 1 file and create 2 files

--- geocodeTree.py
+++ geocodeTree.py
@@ -1,6 +1,7 @@
 import cmritPath as cp
 import osmnx as ox
 import networkx as nx
+import pickle
 
 cf = cp.CmritField()
 
@@ -8,7 +9,17 @@ cf = cp.CmritField()
 class GeocodeTree:
     def __init__(self):
         self.G = cf.graph()
+        self.tree_count = 0
 
     def add_tree(self, coord):
-        self.G.add_node('t-01', x=coord[0], y=coord[1], street_count=3)
+        self.tree_count += 1
+        tree_name = f"t-{self.tree_count}"
+
+        self.G.add_node(tree_name, x=coord[0], y=coord[1], street_count=3)
+
         print(self.G.nodes)
+
+    def save_and_return(self):
+        with open("output/geocode_tree.pkl", "wb") as f:
+            pickle.dump(self.G, f)
+        return self.G
