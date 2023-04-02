2ab09bd85c174f75ecd97b2a4fbb493f4a54b332
amartyanambiar <amartyanambiar@gmail.com>
2023-04-03 01:17:17 +0530

    updated Coordinates for test - Added 4 Nodes

--- test_coordinates.py
+++ test_coordinates.py
@@ -1,21 +1,25 @@
 from coordinates import Coordinates
 
 # basic_science
+basic_science = Coordinates("basic_scince", [12.96626, 77.71211])
 # |
 # V
 tree1 = Coordinates("tree1", [12.96620, 77.71203])
 tree2 = Coordinates("tree2", [12.96612, 77.71179])
 tree3 = Coordinates("tree3", [12.96603, 77.71156])
 # ganesha_statue
+ganesha_statue = Coordinates("ganesha_statue", [12.96598, 77.71148])
 # |
 # V
 tree4 = Coordinates("tree4", [12.96608, 77.71141])
 tree5 = Coordinates("tree5", [12.96643, 77.71129])
 tree6 = Coordinates("tree6", [12.96682, 77.71115])
 # hostel
+hostel_turn = Coordinates("hostel_turn", [12.96696, 77.71111])
 # |
 # V
 tree7 = Coordinates("tree7", [12.96698, 77.71121])
 tree8 = Coordinates("tree8", [12.96698, 77.71121])
 tree9 = Coordinates("tree9", [12.96710, 77.71171])
 # volleyball_court
+volleyball_court = Coordinates("volleyball_court", [12.9671086, 77.7118638])
