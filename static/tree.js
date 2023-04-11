e8f9f00bc19590d7eb1386205a4ae6b42f46758f
amartyanambiar <amartyanambiar@gmail.com>
2023-04-11 11:05:33 +0530

    post start and end nodes

--- static/tree.js
+++ static/tree.js
@@ -1,12 +1,12 @@
-const startSelect = document.querySelector('#start');
-const endSelect = document.querySelector('#end');
+const startSelect = document.querySelector("#start");
+const endSelect = document.querySelector("#end");
 
 // Fetch all nodes and add them as options to the start dropdown menu
-fetch('/fetch-all-nodes')
+fetch("/fetch-all-nodes")
   .then((response) => response.json())
   .then((nodes) => {
     nodes.forEach((node) => {
-      const option = document.createElement('option');
+      const option = document.createElement("option");
       option.innerText = node;
       option.value = node;
       startSelect.appendChild(option);
@@ -14,20 +14,20 @@ fetch('/fetch-all-nodes')
   });
 
 // When a start node is selected, fetch the neighboring nodes and add them as options to the end dropdown menu
-startSelect.addEventListener('change', (event) => {
-    const current_node = event.target.value;
-    console.log(current_node);
-  fetch('/fetch-neighbours', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+startSelect.addEventListener("change", (event) => {
+  const current_node = event.target.value;
+  console.log(current_node);
+  fetch("/fetch-neighbours", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ current_node: current_node }),
   })
     .then((response) => response.json())
-      .then((neighbors) => {
-        console.log(neighbors);
+    .then((neighbors) => {
+      console.log(neighbors);
       endSelect.innerHTML = '<option value="">Select a node</option>'; // Clear the current options in the end dropdown menu
       neighbors.forEach((neighbor) => {
-        const option = document.createElement('option');
+        const option = document.createElement("option");
         option.innerText = neighbor;
         option.value = neighbor;
         endSelect.appendChild(option);
@@ -35,12 +35,21 @@ startSelect.addEventListener('change', (event) => {
     });
 });
 
+const addTreeButton = document.getElementById("add-tree-button");
+const saveButton = document.getElementById("save-button");
 
-const addTreeButton = document.getElementById('add-tree-button');
-const saveButton = document.getElementById('save-button');
+addTreeButton.addEventListener("click", () => {
+  const selectedStart = startSelect.value;
+  const selectedEnd = endSelect.value;
 
-addTreeButton.addEventListener('click', () => {
-  fetch('/add_tree')
+  fetch("/add_tree", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      start_node: selectedStart,
+      end_node: selectedEnd,
+    }),
+  })
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -49,8 +58,8 @@ addTreeButton.addEventListener('click', () => {
     .catch((error) => console.error(error));
 });
 
-saveButton.addEventListener('click', () => {
-  fetch('/save')
+saveButton.addEventListener("click", () => {
+  fetch("/save")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
