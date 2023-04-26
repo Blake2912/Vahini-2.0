4cb4c3960516e1c033df6e3d46ef7f33a38c6dc9
Blake2912 <kcvarun29122001@gmail.com>
2023-04-26 23:22:36 +0530

    The following changes are made:  - Addition of NavMesh for Path finding  - Addition of sensors for obstacle avoidance  - The vehicle will move on the given path automatically with the given destiation

--- unity/Vahini Simulations/Assets/Scripts/VehicleMovement.cs
+++ unity/Vahini Simulations/Assets/Scripts/VehicleMovement.cs
@@ -10,6 +10,16 @@ public class VehicleMovement : MonoBehaviour
     [SerializeField]
     private Transform movePositionTransform;
 
+    [Header("Sensors")]
+    public float sensorLength = 3f;
+    private float frontSensorOffsetz = 1.51f;
+    private float frontSensorOffsety = 0.23f;
+
+    public float frontSideSensorOffsetx = 0.43f;
+    public float frontSideSensorAngle = 30f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +34,45 @@ public class VehicleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = movePositionTransform.position;
+        // agent.destination = movePositionTransform.position;
+        Sensors();
+        
+    }
+
+    private void Sensors(){
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position;
+        sensorStartPos += transform.forward * frontSensorOffsetz;
+        sensorStartPos += transform.up * frontSensorOffsety;
+
+        // Front Sesnor
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+        // Front side right sensor
+        sensorStartPos += transform.right * frontSideSensorOffsetx;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+        // Front Right Angle Sensor
+        if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSideSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+        // Front side left sensor
+        sensorStartPos -= transform.right * 2 * frontSideSensorOffsetx;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+        // Front Left Angle Sensor
+        if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSideSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+        
+
     }
 }
