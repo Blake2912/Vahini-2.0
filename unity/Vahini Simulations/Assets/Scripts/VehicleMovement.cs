782316859a6b589e0a6f866afdf367456c9a96a8
Blake2912 <kcvarun29122001@gmail.com>
2023-05-13 20:22:24 +0530

    The following changes were made:     - Added trees     - Added Wheel Colliders for the vehicle basically suspension     - Added script for path creation      - Addition of NavMesh for Path finding      - Addition of sensors for obstacle avoidance      - The vehicle will move on the given path automatically with the given destiation     - Revamp of new layout     - Add Obstacle avoidance     - Smoothening of turns by using lerp Removed unwanted assets

--- unity/Vahini Simulations/Assets/Scripts/VehicleMovement.cs
+++ unity/Vahini Simulations/Assets/Scripts/VehicleMovement.cs
@@ -14,10 +14,11 @@ public class VehicleMovement : MonoBehaviour
     public WheelCollider wheelFR;
     public WheelCollider wheelBL;
     public WheelCollider wheelBR;
-    public float maxMotorTorque = 80.0f;
+    public float maxMotorTorque = 100.0f;
 
     private float currentSpeed;
     public float maxSpeed = 80.0f;
+    private bool isVehicleStopped = false;
 
     // private NavMeshAgent agent;
     //[SerializeField]
@@ -30,10 +31,12 @@ public class VehicleMovement : MonoBehaviour
 
     public float frontSideSensorOffsetx = 0.43f;
     public float frontSideSensorAngle = 30f;
-    float avoidMultiplier = 0f;
 
 
     private bool avoiding = false;
+
+    private float targetSteerAngle = 0;
+    public float turnSpeed = 5;
 
 
 
@@ -58,10 +61,15 @@ public class VehicleMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // agent.destination = movePositionTransform.position;
-        ApplySteer();
-        Drive();
-        CheckWayPointDistance();
-        Sensors();
+        if(!isVehicleStopped)
+        {
+            ApplySteer();
+            Drive();
+            CheckWayPointDistance();
+            Sensors();
+            LerpToSteerAngle();
+        }
+        
     }
 
     private void Drive()
@@ -69,22 +77,32 @@ public class VehicleMovement : MonoBehaviour
         currentSpeed = 2 * Mathf.PI * wheelBL.radius * wheelBL.rpm * 60/1000;
         if (currentSpeed < maxSpeed){
             wheelBL.motorTorque = maxMotorTorque;
-            wheelBL.motorTorque = maxMotorTorque;
+            wheelBR.motorTorque = maxMotorTorque;
         }
         else{
             wheelBL.motorTorque = 0.0f;
-            wheelBL.motorTorque = 0.0f;
+            wheelBR.motorTorque = 0.0f;
         }
         
     }
 
     private void ApplySteer()
     {
+        if (avoiding)
+        {
+            return;
+        }
         Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
         float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
-        wheelFL.steerAngle = newSteer;
-        wheelFR.steerAngle = newSteer;
+        targetSteerAngle = newSteer;
+    }
 
+    private void Stop()
+    {
+        Debug.Log("Stop Triggered!");
+        isVehicleStopped = true;
+        wheelBL.brakeTorque = 100.0f;
+        wheelBR.brakeTorque = 0.0f;
     }
 
     private void CheckWayPointDistance()
@@ -94,7 +112,8 @@ public class VehicleMovement : MonoBehaviour
             if ( currentNode == nodes.Count-1){
                 // Stop the vehicle
                 // TODO::Remove this
-                currentNode = 0;
+                // currentNode = 0;
+                Stop();
             }
             else{
                 currentNode++;
@@ -108,23 +127,14 @@ public class VehicleMovement : MonoBehaviour
         Vector3 sensorStartPos = transform.position;
         sensorStartPos += transform.forward * frontSensorOffsetz;
         sensorStartPos += transform.up * frontSensorOffsety;
+        float avoidMultiplier = 0f;
         avoiding = false;
 
-        // Front Sesnor
-        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
-        {
-            if(hit.collider.CompareTag("Terrain"))
-            {
-                Debug.DrawLine(sensorStartPos, hit.point);
-                avoiding = true;
-            }
-            
-        }
         // Front side right sensor
         sensorStartPos += transform.right * frontSideSensorOffsetx;
         if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
         {
-            if(hit.collider.CompareTag("Terrain"))
+            if(!hit.collider.CompareTag("Terrain"))
             {
                 Debug.DrawLine(sensorStartPos, hit.point);
                 avoiding = true;
@@ -135,18 +145,18 @@ public class VehicleMovement : MonoBehaviour
         // Front Right Angle Sensor
         else if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSideSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
         {
-            if(hit.collider.CompareTag("Terrain"))
+            if(!hit.collider.CompareTag("Terrain"))
             {
                 Debug.DrawLine(sensorStartPos, hit.point);
                 avoiding = true;
-                avoidMultiplier -= 0.5f;
+                avoidMultiplier -= 0.75f;
             }
         }
         // Front side left sensor
         sensorStartPos -= transform.right * 2 * frontSideSensorOffsetx;
         if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
         {
-            if(hit.collider.CompareTag("Terrain"))
+            if(!hit.collider.CompareTag("Terrain"))
             {
                 Debug.DrawLine(sensorStartPos, hit.point);
                 avoiding = true;
@@ -156,12 +166,46 @@ public class VehicleMovement : MonoBehaviour
         // Front Left Angle Sensor
         if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSideSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
         {
-            if(hit.collider.CompareTag("Terrain"))
+            if(!hit.collider.CompareTag("Terrain"))
             {
                 Debug.DrawLine(sensorStartPos, hit.point);
                 avoiding = true;
-                avoidMultiplier += 0.5f;
+                avoidMultiplier += 0.75f;
             }
         }
+
+        if (avoidMultiplier == 0)
+        {
+            // Front Center Sesnor
+            if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+            {
+                Debug.Log("Inside Front Center Sensor");
+                if(!hit.collider.CompareTag("Terrain"))
+                {
+                    Debug.DrawLine(sensorStartPos, hit.point);
+                    avoiding = true;
+                    if(hit.normal.x < 0)
+                    {
+                        avoidMultiplier = -1;
+                    }
+                    else
+                    {
+                        avoidMultiplier = 1;
+                    }
+                }
+                
+            }
+        }
+        
+
+        if (avoiding){
+            targetSteerAngle = maxSteerAngle * avoidMultiplier;
+        }
+    }
+
+    private void LerpToSteerAngle()
+    {
+        wheelFL.steerAngle = Mathf.Lerp(wheelFL.steerAngle, targetSteerAngle, Time.deltaTime * turnSpeed);
+        wheelFR.steerAngle = Mathf.Lerp(wheelFR.steerAngle, targetSteerAngle, Time.deltaTime * turnSpeed);
     }
 }
